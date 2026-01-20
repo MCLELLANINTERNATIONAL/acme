90735690bf7076c0cc37c80f7e03a0f75f7a88a8
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2026-01-20 14:44:47 +0000

    added pagination

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -2,21 +2,27 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search() {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
   const { replace } = useRouter();
+  const pathname = usePathname();
   
-  function handleSearch(term: string) {
+  
+  const handleSearch = useDebouncedCallback((term) => { 
+    console.log(`Searching... ${term}`);
+
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
+  
   const placeholder = 'Search invoices...';
 
   return (
